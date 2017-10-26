1b40110be8486492ee5dd49026c10e87811b367f
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-10-26 08:47:39 +0200

    Prueba2

---
+++ BoletinPrueba2/src/boletinprueba2/BoletinPrueba2.java
@@ -0,0 +1,11 @@
+package boletinprueba2;
+/**
+ * @author jborrajorodriguez
+ */
+public class BoletinPrueba2 {
+
+    public static void main(String[] args) {
+        System.out.println("Boletin Prueba 2");
+    }
+    
+}
